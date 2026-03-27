de1bfd43240b4b4f6675090099e386e8b8e6d434
Jinyoung Ko <tipss00@naver.com>
2026-03-27 23:44:06 +0900

    fix: header username display4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './hooks/useAuth';
-import Header from './components/common/old_Header';
+import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 import Home from './pages/Home';
 import MapExplore from './pages/MapExplore';
